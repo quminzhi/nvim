773b57313031daaabd5c5279e0512a6994bab4c5
Minzhi <quminzhi@gmail.com>
2024-04-22 10:27:15 -0700

    Sync

--- lua/plugins/plugin-config/nvim-ufo.lua
+++ lua/plugins/plugin-config/nvim-ufo.lua
@@ -32,7 +32,7 @@ require('ufo').setup({
   end,
 
   open_fold_hl_timeout = 150,
-  close_fold_kinds = {'imports', 'comment'},
+  close_fold_kinds_for_ft = {'imports', 'comment'},
   preview = {
     win_config = {
       border = {'', '─', '', '', '', '─', '', ''},
